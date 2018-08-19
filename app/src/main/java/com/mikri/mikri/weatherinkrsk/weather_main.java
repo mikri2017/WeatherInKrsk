75c47fb9e475655a9f89eeb92e4d670392bc851d
MIKRI <2007mik007@mail.ru>
2018-08-19 15:53:37 +0700

    Add opening URL link by clicking Temperature on Widget

--- app/src/main/java/com/mikri/mikri/weatherinkrsk/weather_main.java
+++ app/src/main/java/com/mikri/mikri/weatherinkrsk/weather_main.java
@@ -1,8 +1,13 @@
 package com.mikri.mikri.weatherinkrsk;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
 import android.widget.RemoteViews;
 
 /**
@@ -10,34 +15,66 @@ import android.widget.RemoteViews;
  */
 public class weather_main extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.weather_main);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
+    private static final String SYNC_CLICKED    = "updateWeather";
+    private static final String WIDGET_WEATHER_TEMP_CLICKED    = "goToWebSite";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+        RemoteViews remoteViews;
+        ComponentName watchWidget;
+
+        remoteViews = new RemoteViews(context.getPackageName(), R.layout.weather_main);
+        watchWidget = new ComponentName(context, weather_main.class);
+
+        CharSequence widgetText = "+--.- °C";
+        remoteViews.setTextViewText(R.id.widgetTemp, widgetText);
+        remoteViews.setTextViewText(R.id.widgetPressure, "--");
+        remoteViews.setTextViewText(R.id.widgetHumidity, "--%");
+
+        remoteViews.setOnClickPendingIntent(R.id.imgBtnUpdate, getPendingSelfIntent(context, SYNC_CLICKED));
+        remoteViews.setOnClickPendingIntent(R.id.widgetTemp, getPendingSelfIntent(context, WIDGET_WEATHER_TEMP_CLICKED));
+        appWidgetManager.updateAppWidget(watchWidget, remoteViews);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        super.onReceive(context, intent);
+
+        RemoteViews remoteViews;
+        remoteViews = new RemoteViews(context.getPackageName(), R.layout.weather_main);
+
+        if (SYNC_CLICKED.equals(intent.getAction())) {
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+
+            ComponentName watchWidget;
+
+            watchWidget = new ComponentName(context, weather_main.class);
+
+            CharSequence widgetText = "+24.0 °C";
+            remoteViews.setTextViewText(R.id.widgetTemp, widgetText);
+            remoteViews.setTextViewText(R.id.widgetPressure, "753");
+            remoteViews.setTextViewText(R.id.widgetHumidity, "100%");
+
+            appWidgetManager.updateAppWidget(watchWidget, remoteViews);
+        }
+
+        if (WIDGET_WEATHER_TEMP_CLICKED.equals(intent.getAction())) {
+            try {
+                String siteURL = "http://www.somesite.ru/";
+                Intent webIntent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(siteURL));
+                webIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(webIntent);
+            } catch (RuntimeException e) {
+                // The url is invalid, maybe missing http://
+                e.printStackTrace();
+            }
         }
     }
 
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
+    protected PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
     }
 }
-
